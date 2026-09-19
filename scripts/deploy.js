617b159f62492d9e1861d87b9692ddc06d7a03da
Leruo Motlhamme <leruom@Leruos-MacBook-Air.local>
2026-09-19 13:22:46 +0200

    feat: changed Time to TRST token

---
+++ scripts/deploy.js
@@ -0,0 +1,19 @@
+const { ethers } = require("hardhat");
+
+async function main() {
+  const [deployer] = await ethers.getSigners();
+  console.log("Deploying TRUSTERC20Token with account:", deployer.address);
+
+  const TRUSTERC20Token = await ethers.getContractFactory("TRUSTERC20Token");
+  const token = await TRUSTERC20Token.deploy();
+  await token.deployed();
+
+  console.log("TRUSTERC20Token deployed to:", token.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
